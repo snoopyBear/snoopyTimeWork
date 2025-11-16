0873daa4f5fe1be5c7e97fe33f37decbc3b2c4ee
snoopyBear <idk@mailhaven.su>
2025-11-16 16:37:18 +0100

    Arreglado lo de cambiar el ancho

--- README.md
+++ README.md
@@ -7,13 +7,14 @@ Boton cambiar tamaño el texto
 Añadir salto de linea cuando haya overflow horizontal  
 Hacer funcion general de cambio de estilo con parametro de lo que se cambia para que no haya redundancia  
 La hora  
+Añadir Min-width a la Descripción, y si al arrastrar el widthChanger llega al minWidth que no cambie más  
 
 ## TODO
 
-Añadir Min-width a la Descripción, y si al arrastrar el widthChanger llega al minWidth que no cambie más  
 Poder guardar archivo  
 Sonido cuando suene la cuenta atras a 0 y parar  
 Tema "claro"  
+Traducir a ingles coño que no entienden na
 
 ## TOFIX
 
--- js/general.js
+++ js/general.js
@@ -1,23 +1,22 @@
 const barraTareasBloc = document.getElementById("barraTareasBloc");
+const barraBlocHora = document.getElementById("barraBlocHora");
 const tareasWrapper = document.getElementById("tareasWrapper");
+const cronometroWrapper = document.getElementById("cronometroWrapper");
 const descripciones = document.getElementsByClassName("descripcion");
 const root = document.documentElement;
-
-console.log(getComputedStyle(descripciones[0]).width);
 
 
 barraTareasBloc.addEventListener("mousedown", function(evento) {
 
     let posicionInicial = event.clientX;
     const anchoOriginal = tareasWrapper.offsetWidth;
-
     const anchoOriginalDescripcion = parseInt(getComputedStyle(descripciones[0]).width.replace("px", ""));
     
     document.onmousemove = function() {    
         
         tareasWrapper.style.width = `${anchoOriginal - (posicionInicial - event.clientX)}px`;
         root.style.setProperty("--ancho-descripcion", `${anchoOriginalDescripcion - (posicionInicial - event.clientX)}px`)
-        
+        console.log(tareasWrapper.getBoundingClientRect().width);
     }
 
     document.onmouseup =  function() {
@@ -26,3 +25,24 @@ barraTareasBloc.addEventListener("mousedown", function(evento) {
     }
 
 });
+
+// barraBlocHora.addEventListener("mousedown", function(evento) {
+
+    
+//     let posicionInicial = event.clientX;
+//     const anchoOriginal = cronometroWrapper.offsetWidth;
+//     // const anchoOriginalDescripcion = parseInt(getComputedStyle(descripciones[0]).width.replace("px", ""));
+    
+//     document.onmousemove = function() {    
+        
+//         cronometroWrapper.style.width = `${anchoOriginal + (posicionInicial - event.clientX)}px`;
+//         // root.style.setProperty("--ancho-descripcion", `${anchoOriginalDescripcion - (posicionInicial - event.clientX)}px`)
+//         // console.log(tareasWrapper.getBoundingClientRect().width);
+//     }
+
+//     document.onmouseup =  function() {
+//         document.onmousemove = null;
+//         document.onmouseup = null;
+//     }
+
+// })
